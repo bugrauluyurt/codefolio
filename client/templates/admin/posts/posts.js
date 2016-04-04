4d706baf1a94e6ecc2cf54232e4704f33ab76014
bugrauluyurt <bugra.u@gmail.com>
2016-04-04 23:33:58 +0300

    Installed moment JS

--- client/templates/admin/posts/posts.js
+++ client/templates/admin/posts/posts.js
@@ -16,3 +16,35 @@ Template.add_post.events({
 		return false;
 	}
 });
+
+Template.edit_post.events({
+	'submit .edit_post_form': function(event) {
+		var title = event.target.title.value;
+		var body = event.target.body.value;
+
+		// Update Post
+		Posts.update({_id: this._id},{
+			$set: {
+				title: title,
+				body: body
+			}
+		});
+
+		FlashMessages.sendSuccess('Post Updated');
+		Router.go('/admin/posts');
+
+		// Prevent submit
+		return false;
+	}
+});
+
+Template.list_posts.events({
+	'click .delete_post': function(event) {
+		if (confirm('Are you sure?')) {
+			Posts.remove(this._id);
+			FlashMessages.sendSuccess('Post Deleted');
+			// Prevent submit
+			return false;
+		}
+	}
+});
--- client/templates/helpers.js
+++ client/templates/helpers.js
@@ -36,3 +36,7 @@ Template.layout.events({
 		return false;
 	}
 });
+
+Template.registerHelper('formatDate', function(date) {
+	return moment(date).format('MMM Do YYYY, h:mm:ss a');
+});
